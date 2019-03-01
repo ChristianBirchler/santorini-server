b03edb613237c245559b3ea9390667bdb148afa2
Christian Birchler <christian.birchler2@uzh.ch>
2019-03-01 19:06:38 +0100

    add findById api

--- src/main/java/ch/uzh/ifi/seal/soprafs19/repository/UserRepository.java
+++ src/main/java/ch/uzh/ifi/seal/soprafs19/repository/UserRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository("userRepository")
 public interface UserRepository extends CrudRepository<User, Long> {
+	User findById(long id);
 	User findByUsername(String username);
 	User findByToken(String token);
 }
