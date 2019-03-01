cf78797b097c1293ff7aedf2c6b82360403c3f2d
Christian Birchler <christian.birchler2@uzh.ch>
2019-03-01 14:07:56 +0100

    add Location entity

---
+++ src/main/java/ch/uzh/ifi/seal/soprafs19/entity/Location.java
@@ -0,0 +1,19 @@
+package ch.uzh.ifi.seal.soprafs19.entity;
+
+public class Location {
+
+    private String url;
+
+    public Location(String url){
+        this.url = url;
+    }
+
+    public void setUrl(String url){
+        this.url = url;
+    }
+
+    public String getUrl(){
+        return this.url;
+    }
+
+}
