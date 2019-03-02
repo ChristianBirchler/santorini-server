b0e3ce6d6326ad6d26476747e055182bff9a37ad
Christian Birchler <christian.birchler2@uzh.ch>
2019-03-02 21:25:59 +0100

    implement UserAlreadyExistsException

---
+++ src/main/java/ch/uzh/ifi/seal/soprafs19/exception/UserAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package ch.uzh.ifi.seal.soprafs19.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class UserAlreadyExistsException extends RuntimeException {
+}
