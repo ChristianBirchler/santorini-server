e60d409198a93139ca7a2007fa4ffe75aaf23a03
Christian Birchler <christian.birchler2@uzh.ch>
2019-03-08 16:10:43 +0100

    implement test for credentials check

--- src/test/java/ch/uzh/ifi/seal/soprafs19/service/UserServiceTest.java
+++ src/test/java/ch/uzh/ifi/seal/soprafs19/service/UserServiceTest.java
@@ -2,8 +2,11 @@ package ch.uzh.ifi.seal.soprafs19.service;
 
 import ch.uzh.ifi.seal.soprafs19.Application;
 import ch.uzh.ifi.seal.soprafs19.constant.UserStatus;
+import ch.uzh.ifi.seal.soprafs19.entity.Credentials;
 import ch.uzh.ifi.seal.soprafs19.entity.User;
+import ch.uzh.ifi.seal.soprafs19.exception.InvalidCredentialsException;
 import ch.uzh.ifi.seal.soprafs19.repository.UserRepository;
+import ch.uzh.ifi.seal.soprafs19.response.LoginResponse;
 import ch.uzh.ifi.seal.soprafs19.service.UserService;
 import org.junit.Assert;
 import org.junit.Test;
@@ -83,7 +86,60 @@ public class UserServiceTest {
 
     @Test
     public void checkCredentialsTest(){
-        Assert.fail();
+
+
+        Credentials cred = new Credentials();
+        cred.setUsername("user1");
+        cred.setPassword("password1");
+
+        User user1 = new User();
+        user1.setUsername("user1");
+        user1.setPassword("password1");
+
+        userService.createUser(user1);
+
+        // username and password are correct
+        try {
+
+            LoginResponse loginResponse = userService.checkCredentials(cred);
+            Assert.assertEquals(user1.getToken(), loginResponse.getToken());
+
+
+        } catch (InvalidCredentialsException exception){
+            Assert.fail("Invalid credentials exception");
+        }
+
+
+        // only password is wrong
+        cred.setPassword("password2");
+        try {
+            LoginResponse loginResponse = userService.checkCredentials(cred);
+            Assert.fail("Password should be wrong");
+        } catch (InvalidCredentialsException exception){
+
+        }
+
+        // only username is wrong
+        cred.setPassword("password1");
+        cred.setUsername("user2");
+        try {
+            LoginResponse loginResponse = userService.checkCredentials(cred);
+            Assert.fail("Username should wrong");
+        } catch (InvalidCredentialsException exception){
+
+        }
+
+        // username and password are wrong
+        cred.setPassword("password2");
+        try {
+            LoginResponse loginResponse = userService.checkCredentials(cred);
+            Assert.fail("Username and password should be wrong");
+        } catch (InvalidCredentialsException exception){
+
+        }
+
+
+
     }
 
 
